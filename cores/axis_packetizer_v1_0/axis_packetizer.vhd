8d800fbe8c362da991049ea53561aa257bf7e401
Horacio Arnaldi <lharnaldi@gmail.com>
2016-11-22 16:45:44 -0300

    Change the type in generecis integer->natural.

--- cores/axis_packetizer_v1_0/axis_packetizer.vhd
+++ cores/axis_packetizer_v1_0/axis_packetizer.vhd
@@ -5,8 +5,8 @@ use ieee.numeric_std.all;
 
 entity axis_packetizer is
   generic (
-  AXIS_TDATA_WIDTH : integer := 32;
-  CNTR_WIDTH    : integer    := 32;
+  AXIS_TDATA_WIDTH : natural := 32;
+  CNTR_WIDTH    : natural    := 32;
   CONTINUOUS    : string     := "FALSE"
 );
 port (

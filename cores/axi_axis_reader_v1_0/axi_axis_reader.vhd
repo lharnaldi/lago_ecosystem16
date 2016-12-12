9bcb70c09e8c0646bf5240ad1aa0d7781157274d
Horacio Arnaldi <lharnaldi@gmail.com>
2016-12-12 08:45:30 -0300

    Chenge from asynchonous reset to synchronous.

--- cores/axi_axis_reader_v1_0/axi_axis_reader.vhd
+++ cores/axi_axis_reader_v1_0/axi_axis_reader.vhd
@@ -15,21 +15,21 @@ port (
   
   -- Slave side
   s_axi_awaddr 	: in  std_logic_vector(AXI_ADDR_WIDTH-1 downto 0);  -- AXI4-Lite slave: Write address
-  s_axi_awvalid	: in  std_logic; 			              -- AXI4-Lite slave: Write address valid
-  s_axi_awready	: out std_logic; 			              -- AXI4-Lite slave: Write address ready
+  s_axi_awvalid	: in  std_logic; 			            -- AXI4-Lite slave: Write address valid
+  s_axi_awready	: out std_logic; 			            -- AXI4-Lite slave: Write address ready
   s_axi_wdata	: in  std_logic_vector(AXI_DATA_WIDTH-1 downto 0);  -- AXI4-Lite slave: Write data
-  s_axi_wvalid	: in  std_logic;  				      -- AXI4-Lite slave: Write data valid
-  s_axi_wready	: out std_logic;  				      -- AXI4-Lite slave: Write data ready
-  s_axi_bresp	: out std_logic_vector(1 downto 0);   		      -- AXI4-Lite slave: Write response
-  s_axi_bvalid	: out std_logic;  				      -- AXI4-Lite slave: Write response valid
-  s_axi_bready	: in  std_logic;  				      -- AXI4-Lite slave: Write response ready
+  s_axi_wvalid	: in  std_logic;  				    -- AXI4-Lite slave: Write data valid
+  s_axi_wready	: out std_logic;  				    -- AXI4-Lite slave: Write data ready
+  s_axi_bresp	: out std_logic_vector(1 downto 0);   		    -- AXI4-Lite slave: Write response
+  s_axi_bvalid	: out std_logic;  				    -- AXI4-Lite slave: Write response valid
+  s_axi_bready	: in  std_logic;  				    -- AXI4-Lite slave: Write response ready
   s_axi_araddr	: in  std_logic_vector(AXI_ADDR_WIDTH-1 downto 0);  -- AXI4-Lite slave: Read address
-  s_axi_arvalid	: in  std_logic; 				      -- AXI4-Lite slave: Read address valid
-  s_axi_arready	: out std_logic; 				      -- AXI4-Lite slave: Read address ready
-  s_axi_rdata	: out std_logic_vector(AXI_DATA_WIDTH-1 downto 0);    -- AXI4-Lite slave: Read data
-  s_axi_rresp	: out std_logic_vector(1 downto 0);   	      -- AXI4-Lite slave: Read data response
-  s_axi_rvalid	: out std_logic;  				      -- AXI4-Lite slave: Read data valid
-  s_axi_rready	: in  std_logic;  				      -- AXI4-Lite slave: Read data ready
+  s_axi_arvalid	: in  std_logic; 				    -- AXI4-Lite slave: Read address valid
+  s_axi_arready	: out std_logic; 				    -- AXI4-Lite slave: Read address ready
+  s_axi_rdata	: out std_logic_vector(AXI_DATA_WIDTH-1 downto 0);  -- AXI4-Lite slave: Read data
+  s_axi_rresp	: out std_logic_vector(1 downto 0);   	            -- AXI4-Lite slave: Read data response
+  s_axi_rvalid	: out std_logic;  				    -- AXI4-Lite slave: Read data valid
+  s_axi_rready	: in  std_logic;  				    -- AXI4-Lite slave: Read data ready
   
   -- Slave side
   s_axis_tready : out std_logic;
@@ -45,14 +45,16 @@ signal int_rdata_reg, int_rdata_next : std_logic_vector(AXI_DATA_WIDTH-1 downto 
 
 begin
 
-  process(aclk, aresetn)
+  process(aclk)
   begin
+    if rising_edge(aclk) then 
     if (aresetn = '0') then
       int_rvalid_reg <= '0';
       int_rdata_reg <= (others => '0');
-    elsif rising_edge(aclk) then 
+    else
       int_rvalid_reg <= int_rvalid_next;
       int_rdata_reg <= int_rdata_next;
+    end if;
     end if;
   end process;
   

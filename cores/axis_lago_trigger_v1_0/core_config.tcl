99ed08d41cfb7bb44466a921eea903f4180a03a7
Horacio Arnaldi <lharnaldi@gmail.com>
2016-12-16 12:09:44 -0300

    Change the trig_lvl and subtrig_lvl ports.

--- cores/axis_lago_trigger_v1_0/core_config.tcl
+++ cores/axis_lago_trigger_v1_0/core_config.tcl
@@ -1,4 +1,4 @@
-set display_name {AXI4-Stream Trigger}
+set display_name {AXI4-Stream LAGO Trigger}
 
 set core [ipx::current_core]
 
@@ -6,7 +6,11 @@ set_property DISPLAY_NAME $display_name $core
 set_property DESCRIPTION $display_name $core
 
 core_parameter AXIS_TDATA_WIDTH {AXIS TDATA WIDTH} {Width of the S_AXIS data bus.}
-core_parameter AXIS_TDATA_SIGNED {AXIS_TDATA_SIGNED} {If TRUE, the S_AXIS data are signed values.}
+core_parameter CLK_FREQ {CLK_FREQ} {Clock frequency of the core.}
+core_parameter ADC_DATA_WIDTH {ADC_DATA_WIDTH} {Number of ADC data bits.}
+core_parameter DATA_ARRAY_LENGTH {DATA_ARRAY_LENGTH} {Length of the data array.}
+core_parameter METADATA_ARRAY_LENGTH {METADATA_ARRAY_LENGTH} {Length of the metadata array.}
+core_parameter SUBTRIG_ARRAY_LENGTH {SUBTRIG_ARRAY_LENGTH} {Length of the sub-trigger array.}
 
 set bus [ipx::get_bus_interfaces -of_objects $core s_axis]
 set_property NAME S_AXIS $bus

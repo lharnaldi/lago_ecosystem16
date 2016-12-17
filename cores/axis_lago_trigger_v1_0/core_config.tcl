2bcf1eeb0e0072370beb993a18e9e6fed7842a2e
Horacio Arnaldi lharnaldi <Horacio Arnaldi lharnaldi@gmail.com>
2016-12-17 20:29:17 -0300

    Now lago project compile. It is necessary to make some test with the rp board to check proper functionality.

--- cores/axis_lago_trigger_v1_0/core_config.tcl
+++ cores/axis_lago_trigger_v1_0/core_config.tcl
@@ -12,10 +12,15 @@ core_parameter DATA_ARRAY_LENGTH {DATA_ARRAY_LENGTH} {Length of the data array.}
 core_parameter METADATA_ARRAY_LENGTH {METADATA_ARRAY_LENGTH} {Length of the metadata array.}
 core_parameter SUBTRIG_ARRAY_LENGTH {SUBTRIG_ARRAY_LENGTH} {Length of the sub-trigger array.}
 
+set bus [ipx::get_bus_interfaces -of_objects $core m_axis]
+set_property NAME M_AXIS $bus
+set_property INTERFACE_MODE master $bus
+
 set bus [ipx::get_bus_interfaces -of_objects $core s_axis]
 set_property NAME S_AXIS $bus
 set_property INTERFACE_MODE slave $bus
 
 set bus [ipx::get_bus_interfaces aclk]
 set parameter [ipx::get_bus_parameters -of_objects $bus ASSOCIATED_BUSIF]
-set_property VALUE S_AXIS $parameter
+set_property VALUE M_AXIS:S_AXIS $parameter
+

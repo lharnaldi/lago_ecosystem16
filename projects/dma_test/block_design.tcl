74335f6d7013a1ebaa6bc86e9a0dae51384c955c
Horacio Arnaldi <lharnaldi@gmail.com>
2017-01-11 18:21:44 -0300

    This version was tested and works ok. There are some issues with the c code to review jet.

--- projects/dma_test/block_design.tcl
+++ projects/dma_test/block_design.tcl
@@ -64,6 +64,7 @@ cell xilinx.com:ip:xlslice:1.0 slice_0 {
 # Create DMA
 cell xilinx.com:ip:axi_dma:7.1 axi_dma_0 {
   c_sg_include_stscntrl_strm 0 
+  c_include_sg 0
   c_include_mm2s 0 
   c_include_s2mm 1
 } {
@@ -91,13 +92,31 @@ cell xilinx.com:ip:axis_clock_converter:1.1 fifo_0 {} {
 
 # Create data FIFO
 cell xilinx.com:ip:axis_data_fifo:1.1 fifo_1 {
-  FIFO_DEPTH 4096
+  FIFO_DEPTH 16384
   FIFO_MODE 2
 } {
-  S_AXIS fifo_0/M_AXIS
   M_AXIS axi_dma_0/S_AXIS_S2MM
   s_axis_aclk ps_0/FCLK_CLK0
   s_axis_aresetn rst_0/peripheral_aresetn
+}
+
+# Create the tlast generator
+cell labdpr:user:axis_tlast_gen:1.0 tlast_gen_0 {
+  AXIS_TDATA_WIDTH 32
+  PKT_CNTR_BITS 16
+} {
+  M_AXIS fifo_1/S_AXIS
+  S_AXIS fifo_0/M_AXIS
+  aclk ps_0/FCLK_CLK0
+  aresetn rst_0/peripheral_aresetn
+}
+
+# Create xlconstant
+cell xilinx.com:ip:xlconstant:1.1 const_1 {
+ CONST_VAL 8192
+ CONST_WIDTH 16
+} {
+ dout tlast_gen_0/pkt_length
 }
 
 # Create all required interconnections
@@ -111,35 +130,11 @@ set_property OFFSET 0x40400000 [get_bd_addr_segs ps_0/Data/SEG_axi_dma_0_reg]
 
 # Create all required interconnections
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
-  Master /axi_dma_0/M_AXI_SG
+  Master /axi_dma_0/M_AXI_S2MM
   Clk Auto
 } [get_bd_intf_pins ps_0/S_AXI_HP0]
 
-set_property RANGE 512M [get_bd_addr_segs axi_dma_0/Data_SG/SEG_ps_0_HP0_DDR_LOWOCM]
-
-# Create all required interconnections
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
-#  Slave /ps_0/S_AXI_HP0
-#  Clk Auto
-#} [get_bd_intf_pins axi_dma_0/M_AXI_MM2S]
-#
-#set_property RANGE 512M [get_bd_addr_segs axi_dma_0/Data_MM2S/SEG_ps_0_HP0_DDR_LOWOCM]
-
-# Create all required interconnections
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
-  Slave /ps_0/S_AXI_HP0
-  Clk Auto
-} [get_bd_intf_pins axi_dma_0/M_AXI_S2MM]
-
 set_property RANGE 512M [get_bd_addr_segs axi_dma_0/Data_S2MM/SEG_ps_0_HP0_DDR_LOWOCM]
 
-startgroup
-set_property -dict [list CONFIG.c_include_sg {0} CONFIG.c_sg_include_stscntrl_strm {0}] [get_bd_cells axi_dma_0]
-endgroup
-
-startgroup
-set_property -dict [list CONFIG.NUM_SI {1} CONFIG.NUM_MI {1}] [get_bd_cells axi_mem_intercon]
-endgroup
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Slave "/ps_0/S_AXI_HP0" Clk "Auto" }  [get_bd_intf_pins axi_dma_0/M_AXI_S2MM]
-
 assign_bd_address [get_bd_addr_segs ps_0/S_AXI_HP0/HP0_DDR_LOWOCM]
+

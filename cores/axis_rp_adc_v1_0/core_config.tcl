803050cc1a713fe217236058297764ad7bee3346
lharnaldi <lharnaldi@gmail.com>
2017-03-23 13:24:11 -0300

    Change in the adc interface to take into account the use of the PLL for clock.

--- cores/axis_rp_adc_v1_0/core_config.tcl
+++ cores/axis_rp_adc_v1_0/core_config.tcl
@@ -12,8 +12,6 @@ set bus [ipx::get_bus_interfaces -of_objects $core m_axis]
 set_property NAME M_AXIS $bus
 set_property INTERFACE_MODE master $bus
 
-set bus [ipx::get_bus_interfaces adc_clk]
+set bus [ipx::get_bus_interfaces aclk]
 set parameter [ipx::add_bus_parameter ASSOCIATED_BUSIF $bus]
 set_property VALUE M_AXIS $parameter
-set parameter [ipx::add_bus_parameter FREQ_HZ $bus]
-set_property VALUE 125000000 $parameter

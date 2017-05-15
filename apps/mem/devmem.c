30b0ba43225de66187f07458583b3fa154672a74
lharnaldi <lharnaldi@gmail.com>
2017-05-15 11:24:03 -0300

    Some TAB removals in devmem.c and add the file gpio-dev-mem-test.c for gpio testings

--- apps/mem/devmem.c
+++ apps/mem/devmem.c
@@ -1,40 +1,30 @@
 /*
- *  * devmem2.c: Simple program to read/write from/to any location in memory.
- *   *
- *    *  Copyright (C) 2000, Jan-Derk Bakker (jdb@lartmaker.nl)
- *     *
- *      *
- *       * This software has been developed for the LART computing board
- *        * (http://www.lart.tudelft.nl/). The development has been sponsored by
- *         * the Mobile MultiMedia Communications (http://www.mmc.tudelft.nl/)
- *          * and Ubiquitous Communications (http://www.ubicom.tudelft.nl/)
- *           * projects.
- *            *
- *             *
- *              * This program is free software; you can redistribute it and/or
- *              modify
- *               * it under the terms of the GNU General Public License as
- *               published by
- *                * the Free Software Foundation; either version 2 of the
- *                License, or
- *                 * (at your option) any later version.
- *                  *
- *                   * This program is distributed in the hope that it will be
- *                   useful,
- *                    * but WITHOUT ANY WARRANTY; without even the implied
- *                    warranty of
- *                     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *                     See the
- *                      * GNU General Public License for more details.
- *                       * 
- *                        * You should have received a copy of the GNU General
- *                        Public License
- *                         * along with this program; if not, write to the Free
- *                         Software
- *                          * Foundation, Inc., 59 Temple Place, Suite 330,
- *                          Boston, MA  02111-1307  USA
- *                           *
- *                            */
+* devmem2.c: Simple program to read/write from/to any location in memory.
+*
+*  Copyright (C) 2000, Jan-Derk Bakker (jdb@lartmaker.nl)
+*
+*
+* This software has been developed for the LART computing board
+* (http://www.lart.tudelft.nl/). The development has been sponsored by
+* the Mobile MultiMedia Communications (http://www.mmc.tudelft.nl/)
+* and Ubiquitous Communications (http://www.ubicom.tudelft.nl/)
+* projects.
+*
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as  published by
+* the Free Software Foundation; either version 2 of the   License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+* */
 
 #include <stdio.h>
 #include <stdlib.h>

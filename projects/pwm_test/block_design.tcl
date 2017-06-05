0d8914e32240730faf0e3dbc7af6dfae1edc53fb
lharnaldi <lharnaldi@gmail.com>
2017-06-05 14:41:28 -0300

    Change the pwm_gen core. The maximum value is 3000000 for 1.8V at output.

--- projects/pwm_test/block_design.tcl
+++ projects/pwm_test/block_design.tcl
@@ -5,33 +5,94 @@ cell xilinx.com:ip:proc_sys_reset:5.0 rst_0
 
 # Create axi_cfg_register
 cell labdpr:user:axi_cfg_register:1.0 cfg_0 {
-  CFG_DATA_WIDTH 64
+  CFG_DATA_WIDTH 160
   AXI_ADDR_WIDTH 32
   AXI_DATA_WIDTH 32
 }
 
 # Create xlslice
 cell xilinx.com:ip:xlslice:1.0 slice_1 {
-  DIN_WIDTH 64 DIN_FROM 0 DIN_TO 0 DOUT_WIDTH 1
+  DIN_WIDTH 160 DIN_FROM 0 DIN_TO 0 DOUT_WIDTH 1
 } {
   Din cfg_0/cfg_data
 }
 
-# Create xlslice
+# Create xlslice. 
 cell xilinx.com:ip:xlslice:1.0 slice_2 {
-  DIN_WIDTH 64 DIN_FROM 58 DIN_TO 32 DOUT_WIDTH 27
+  DIN_WIDTH 160 DIN_FROM 55 DIN_TO 32 DOUT_WIDTH 24
 } {
   Din cfg_0/cfg_data
+}
+
+# Create xlslice.
+cell xilinx.com:ip:xlslice:1.0 slice_3 {
+  DIN_WIDTH 160 DIN_FROM 87 DIN_TO 64 DOUT_WIDTH 24
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice.
+cell xilinx.com:ip:xlslice:1.0 slice_4 {
+  DIN_WIDTH 160 DIN_FROM 119 DIN_TO 96 DOUT_WIDTH 24
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice. 
+cell xilinx.com:ip:xlslice:1.0 slice_5 {
+  DIN_WIDTH 160 DIN_FROM 151 DIN_TO 128 DOUT_WIDTH 24
+} {
+  Din cfg_0/cfg_data
+}
+
+#Create concatenator
+cell xilinx.com:ip:xlconcat:2.1 xlconcat_0 {
+  NUM_PORTS 4
+} {
+  dout dac_pwm_o
+}
+#Create PWM generator
+cell labdpr:user:pwm_gen:1.0 pwm_gen_0 {
+  DATA_WIDTH 24
+  MAX_CNT 156
+} {
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_1/Dout
+  cfg_i slice_2/Dout
+  pwm_o xlconcat_0/In0
 }
 
 #Create PWM generator
-cell labdpr:user:pwm_gen:1.0 pwm_gen_0 {
-  TDATA_WIDTH 27
+cell labdpr:user:pwm_gen:1.0 pwm_gen_1 {
+  DATA_WIDTH 24
+  MAX_CNT 156
 } {
-  aclk pll_0/clk_out1
+  aclk ps_0/FCLK_CLK0
   aresetn slice_1/Dout
-  cfg_i slice_2/Dout
-  pwm_o dac_pwm_o
+  cfg_i slice_3/Dout
+  pwm_o xlconcat_0/In1
+}
+
+#Create PWM generator
+cell labdpr:user:pwm_gen:1.0 pwm_gen_2 {
+  DATA_WIDTH 24
+  MAX_CNT 156
+} {
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_1/Dout
+  cfg_i slice_4/Dout
+  pwm_o xlconcat_0/In2
+}
+
+#Create PWM generator
+cell labdpr:user:pwm_gen:1.0 pwm_gen_3 {
+  DATA_WIDTH 24
+  MAX_CNT 156
+} {
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_1/Dout
+  cfg_i slice_5/Dout
+  pwm_o xlconcat_0/In3
 }
 
 # Create all required interconnections

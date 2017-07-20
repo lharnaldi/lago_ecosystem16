2d8effc8135e3b94c36b61c993d45604719e6158
lharnaldi <lharnaldi@gmail.com>
2017-07-20 12:17:09 -0300

    Change the connection between the pll_0 block and the dac block. Now clock is provided by the pll_0 block.

--- projects/dac_test/block_design.tcl
+++ projects/dac_test/block_design.tcl
@@ -1,15 +1,5 @@
 source projects/packetizer_test/block_design.tcl
 
-# Create clk_wiz
-cell xilinx.com:ip:clk_wiz:5.3 pll_0 {
-  PRIM_IN_FREQ.VALUE_SRC USER
-  PRIM_IN_FREQ 125.0
-  CLKOUT1_USED true
-  CLKOUT1_REQUESTED_OUT_FREQ 250.0
-} {
-  clk_in1 adc_0/adc_clk
-}
-
 # Create dds_compiler
 cell xilinx.com:ip:dds_compiler:6.0 dds_0 {
   DDS_CLOCK_RATE 125
@@ -19,13 +9,13 @@ cell xilinx.com:ip:dds_compiler:6.0 dds_0 {
   HAS_PHASE_OUT false
   OUTPUT_FREQUENCY1 0.9765625
 } {
-  aclk adc_0/adc_clk
+  aclk pll_0/clk_out1
 }
 
 # Create axis_rp_dac
 cell labdpr:user:axis_rp_dac:1.0 dac_0 {} {
-  aclk adc_0/adc_clk
-  ddr_clk pll_0/clk_out1
+  aclk pll_0/clk_out1
+  ddr_clk pll_0/clk_out2
   locked pll_0/locked
   S_AXIS dds_0/M_AXIS_DATA
   dac_clk dac_clk_o

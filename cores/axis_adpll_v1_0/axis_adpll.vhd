3f04c2b0b3f944d18e3f7f28c3561d0cae0d4808
lharnaldi <lharnaldi@gmail.com>
2017-08-03 10:38:36 -0300

    Added the mux to select the source for reference signal. Added the m_axis interface.

--- cores/axis_adpll_v1_0/axis_adpll.vhd
+++ cores/axis_adpll_v1_0/axis_adpll.vhd
@@ -5,28 +5,27 @@ use ieee.numeric_std.all;
 
 entity adpll is
 generic (
-  AXIS_TDATA_WIDTH: natural := 32
+  AXIS_TDATA_WIDTH: natural := 32;
+  ADC_DATA_WIDTH  : natural := 14
 );
 port(
   aclk         : in std_logic;
   aresetn      : in std_logic;
   kp_i         : in std_logic_vector(32-1 downto 0); --proportional gain 
   ki_i         : in std_logic_vector(32-1 downto 0); --integral gain
+
+  gen_en_i     : in std_logic; 
+  freq_i       : in std_logic_vector(32-1 downto 0); --frequency value for generator
   ref_i        : in std_logic; 
 --  phase_I    : out std_logic_vector (14-1 downto 0);
 --  phase_Q    : out std_logic_vector (14-1 downto 0);
   locked_o     : out std_logic;
-  sin_o        : out std_logic_vector(14-1 downto 0);
-  cos_o        : out std_logic_vector(14-1 downto 0);
+--  sin_o        : out std_logic_vector(14-1 downto 0);
+--  cos_o        : out std_logic_vector(14-1 downto 0);
 
   -- Master side
   m_axis_tdata : out std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0);
-  m_axis_tlast : out std_logic;
-  m_axis_tvalid: out std_logic;
-
-  -- Slave side
-  s_axis_tready: out std_logic;
-  s_axis_tdata : in std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0)
+  m_axis_tvalid: out std_logic
 
 );
 end adpll;
@@ -35,9 +34,11 @@ architecture rtl of adpll is
 
 --constant C_GAIN: std_logic_vector(32-1 downto 0):= std_logic_vector(to_unsigned(2097152, 32)); --"00000000001000000000000000000000"; 8192.00
 --constant I_GAIN: std_logic_vector(32-1 downto 0):=std_logic_vector(to_unsigned(32,32));
+constant PADDING_WIDTH : natural := AXIS_TDATA_WIDTH/2 - ADC_DATA_WIDTH;
 
 --input registers for loop filter parameters
 signal kp_reg, kp_next : std_logic_vector(32-1 downto 0);
+signal ki_reg, ki_next : std_logic_vector(32-1 downto 0);
 
 --PSD realted signals
 signal reset: std_logic := '0';
@@ -51,13 +52,27 @@ signal dds_fbk_reg, dds_fbk_next   : std_logic_vector(32-1 downto 0);
 signal dds_fbk                     : std_logic_vector(32-1 downto 0);
 
 --DDS related signals
-signal phase_acc_reg, phase_acc_next: std_logic_vector(49 downto 0);
-signal phase_step: std_logic_vector(49 downto 0) := (others => '0');
+signal phase_acc_reg, phase_acc_next: std_logic_vector(50-1 downto 0);
+signal phase_step: std_logic_vector(50-1 downto 0) := (others => '0');
 signal dds_sync  : std_logic;
 
-signal lut_addr : std_logic_vector(14-1 downto 0);
+signal lut_addr  : std_logic_vector(14-1 downto 0);
+signal sine      : std_logic_vector(14-1 downto 0);
+signal cosine    : std_logic_vector(14-1 downto 0);
+
+signal gen_s     : std_logic;
+signal ref_s     : std_logic;
 
 begin
+
+--Master side
+m_axis_tvalid <= '1';
+--padding to m_axis_tdata size
+m_axis_tdata <= ((PADDING_WIDTH-1) downto 0 => sine(ADC_DATA_WIDTH-1)) & sine & ((PADDING_WIDTH-1) downto 0 => cosine(ADC_DATA_WIDTH-1)) & cosine;
+
+-- mux the input reference
+ref_s <= gen_s when gen_en_i = '1' else
+         ref_i;
 
 --register the input loop filter parameters
 process(aclk)
@@ -77,14 +92,14 @@ end process;
 kp_next <= kp_i;
 ki_next <= ki_i;
 
-reset      <= comp_up_reg and comp_dn_reg;
+reset   <= comp_up_reg and comp_dn_reg;
 
 --PFD phase comparator flip flops
-process(ref_i, reset)
+process(ref_s, reset)
 begin
  if (reset = '1') then
   comp_up_reg <= '0';
- elsif rising_edge(ref_i)  then
+ elsif rising_edge(ref_s)  then
   comp_up_reg <= comp_up_next;
 end if;
 end process;
@@ -148,10 +163,10 @@ end process;
  
  lut: entity work.sincos_lut_14 
    port map( 
-     clk_i   => clk_i,
+     clk_i   => aclk,
      addr_i  => lut_addr,
-     sin     => sin_o,
-     cos     => cos_o
+     sin     => sine,
+     cos     => cosine
    );
  
  --Locked state detector
@@ -159,9 +174,17 @@ end process;
    port map(
      aclk     => aclk,
      aresetn  => aresetn,
-     ref_i    => ref_i,
+     ref_i    => ref_s,
      dds_sync_i => dds_sync,
      locked_o => locked_o
+);
+
+ --clock generator
+ clock_gen: entity work.clk_gen
+   port map(
+     aclk   => aclk,
+     freq_i => freq_i,
+     div_o  => gen_s
 );
 
 end rtl;

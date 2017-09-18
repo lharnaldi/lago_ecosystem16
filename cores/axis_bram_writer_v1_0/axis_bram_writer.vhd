6d51b9394ece073a8c29e225612e26c937c04d21
lharnaldi <lharnaldi@gmail.com>
2017-09-18 16:43:25 -0300

    Added the axis_bram_* cores

---
+++ cores/axis_bram_writer_v1_0/axis_bram_writer.vhd
@@ -0,0 +1,78 @@
+library ieee;
+
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity axis_bram_writer is
+  generic (
+  BRAM_ADDR_WIDTH   : natural := 10;
+  BRAM_DATA_WIDTH   : natural := 32;
+  AXIS_TDATA_WIDTH  : natural := 32
+  );
+  port (
+  -- System signals
+  aclk             : in std_logic;
+  aresetn          : in std_logic;
+  
+  sts_data         : out std_logic_vector(BRAM_ADDR_WIDTH-1 downto 0);
+
+  -- Slave side
+  s_axis_tready    : out std_logic;
+  s_axis_tdata     : in std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0);
+  s_axis_tvalid    : in std_logic
+
+  -- BRAM port
+  bram_porta_clk   : out std_logic;
+  bram_porta_rst   : out std_logic;
+  bram_porta_addr  : out std_logic_vector(BRAM_ADDR_WIDTH-1 downto 0);
+  bram_porta_wrdata: out std_logic_vector(BRAM_DATA_WIDTH-1 downto 0);
+  bram_porta_we    : out std_logic_vector(BRAM_DATA_WIDTH/8-1 downto 0)
+);
+end axis_bram_writer;
+
+architecture rtl of axis_bram_writer is
+  signal int_addr_reg, int_addr_next : std_logic_vector(BRAM_ADDR_WIDTH-1 downto 0);
+  reg [BRAM_ADDR_WIDTH-1:0] int_addr_reg, int_addr_next;
+  reg int_enbl_reg, int_enbl_next;
+
+  always @(posedge aclk)
+  begin
+    if(~aresetn)
+    begin
+      int_addr_reg <= {(BRAM_ADDR_WIDTH){1'b0}};
+      int_enbl_reg <= 1'b0;
+    end
+    else
+    begin
+      int_addr_reg <= int_addr_next;
+      int_enbl_reg <= int_enbl_next;
+    end
+  end
+
+  always @*
+  begin
+    int_addr_next = int_addr_reg;
+    int_enbl_next = int_enbl_reg;
+
+    if(~int_enbl_reg)
+    begin
+      int_enbl_next = 1'b1;
+    end
+
+    if(s_axis_tvalid & int_enbl_reg)
+    begin
+      int_addr_next = int_addr_reg + 1'b1;
+    end
+  end
+
+  assign sts_data = int_addr_reg;
+
+  assign s_axis_tready = int_enbl_reg;
+
+  assign bram_porta_clk = aclk;
+  assign bram_porta_rst = ~aresetn;
+  assign bram_porta_addr = int_addr_reg;
+  assign bram_porta_wrdata = s_axis_tdata;
+  assign bram_porta_we = {(BRAM_DATA_WIDTH/8){s_axis_tvalid & int_enbl_reg}};
+
+endmodule

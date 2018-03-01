0aa6d7bea14242dcef93a3098272da94a1fa6e6d
lharnaldi <lharnaldi@gmail.com>
2018-03-01 09:10:01 -0300

    Added the ports.xdc file that may be in the corresponding project.

--- scripts/project.tcl
+++ scripts/project.tcl
@@ -84,7 +84,7 @@ if {[llength $files] > 0} {
   add_files -norecurse $files
 }
 
-set files [glob -nocomplain cfg/*.xdc]
+set files [glob -nocomplain cfg/*.xdc projects/$project_name/*.xdc]
 if {[llength $files] > 0} {
   add_files -norecurse -fileset constrs_1 $files
 }

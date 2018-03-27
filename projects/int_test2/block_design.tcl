f1c7951ebd415d94cf0aa34f112dd08cb888883d
lharnaldi <lharnaldi@gmail.com>
2018-03-27 08:08:16 -0300

    Added the intc core to test interruptions.

--- projects/int_test2/block_design.tcl
+++ projects/int_test2/block_design.tcl
@@ -21,6 +21,25 @@ cell labdpr:user:axis_rp_adc:1.0 adc_0 {} {
 cell labdpr:user:int_counter:1.0 counter_0 {} {
   aclk ps_0/FCLK_CLK0
   aresetn rst_0/peripheral_aresetn
-  int_o ps_0/IRQ_F2P
 }
+
+# Create axi_intc
+cell xilinx.com:ip:axi_intc:4.1 axi_intc_0 {
+  C_IRQ_CONNECTION 1
+  C_S_AXI_ACLK_FREQ_MHZ 143
+  C_PROCESSOR_CLK_FREQ_MHZ 143
+} {
+  intr counter_0/int_o
+  irq ps_0/IRQ_F2P
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins axi_intc_0/s_axi]
+
+set_property RANGE 4K [get_bd_addr_segs ps_0/Data/SEG_axi_intc_0_Reg]
+set_property OFFSET 0x40000000 [get_bd_addr_segs ps_0/Data/SEG_axi_intc_0_Reg]
+
 

8b4085fbe02bb7e63cf2f0f537c604370122f511
lharnaldi <lharnaldi@gmail.com>
2018-04-21 17:59:53 -0300

    Functional working core. It was tested and is now correctly working.

--- cores/axis_counter_v1_0/axis_counter.vhd
+++ cores/axis_counter_v1_0/axis_counter.vhd
@@ -18,15 +18,16 @@ port (
   -- Master side
   m_axis_tdata       : out std_logic_vector(AXIS_TDATA_WIDTH-1 downto 0);
   m_axis_tlast       : out std_logic;
+  m_axis_tready      : in std_logic;
   m_axis_tvalid      : out std_logic
 );
 end axis_counter;
 
 architecture rtl of axis_counter is
 
-  signal cntr_reg, cntr_next : unsigned(CNTR_WIDTH-1 downto 0);
-  signal int_comp_wire               : std_logic;
-  signal int_tlast_wire              : std_logic;
+  signal cntr_reg, cntr_next   : unsigned(CNTR_WIDTH-1 downto 0);
+  signal tlast_reg, tlast_next : std_logic;
+  signal comp_reg, comp_next  : std_logic;
 
 begin
 
@@ -35,22 +36,26 @@ begin
     if (rising_edge(aclk)) then
     if(aresetn = '0') then
       cntr_reg <= (others => '0');
+      tlast_reg <= '0';
+      comp_reg <= '0';
     else
       cntr_reg <= cntr_next;
+      tlast_reg <= tlast_next;
+      comp_reg <= comp_next;
     end if;
     end if;
   end process;
 
-  int_tlast_wire <= '1' when (cntr_reg = unsigned(cfg_data)-1) else '0';
+  tlast_next <= '1' when (cntr_reg = unsigned(cfg_data)-1) else '0';
 
-  int_comp_wire <= '1' when (cntr_reg < unsigned(cfg_data)) else '0';
+  comp_next <= '0' when (cntr_reg = unsigned(cfg_data)) else '1';
 
-  cntr_next <= cntr_reg + 1 when (int_comp_wire = '1') else
-               (others => '0') when (int_comp_wire = '0') else --reset
+  cntr_next <= cntr_reg + 1 when ((comp_reg = '1') and (m_axis_tready = '1')) else
+               (others => '0') when (comp_reg = '0') else --reset
                cntr_reg;
 
   m_axis_tdata <= std_logic_vector(resize(cntr_reg, m_axis_tdata'length));
-  m_axis_tlast <= int_tlast_wire;
-  m_axis_tvalid <= int_comp_wire;
+  m_axis_tlast <= tlast_reg;
+  m_axis_tvalid <= comp_reg;
 
 end rtl;

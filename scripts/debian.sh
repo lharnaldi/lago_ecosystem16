4df54a37e78813a57e6a4b5d9d17d811dd740d5a
lagoprojectrp <lharnaldi@gmail.com>
2022-04-27 12:49:53 -0300

    Cambio la version de linux a 107

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -4,12 +4,12 @@ boot_dir=`mktemp -d /tmp/BOOT.XXXXXXXXXX`
 root_dir=`mktemp -d /tmp/ROOT.XXXXXXXXXX`
 
 linux_dir=tmp/linux-5.10
-linux_ver=5.10.46-xilinx
+linux_ver=5.10.107-xilinx
 
 # Choose mirror automatically, depending the geographic and network location
 mirror=http://deb.debian.org/debian
 
-distro=buster
+distro=stretch
 arch=armhf
 
 passwd=escondido
